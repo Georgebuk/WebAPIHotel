483abaaf73b64acafdb5ceda6d6d259348cf5131
George <George@DESKTOP-E0782RD>
2019-02-11 16:03:45 +0000

    Add project files.

---
+++ WebAPIHotel/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace WebAPIHotel.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
