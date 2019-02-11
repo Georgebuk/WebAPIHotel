97a2a45490cf8ede67e795ceef24da968e70b5b8
George <George@DESKTOP-E0782RD>
2019-02-11 16:24:36 +0000

    Added new controller for booking requests.
    Added SQL Handler for all SQL Requests
    
    Added reference to class library for hotel projects

---
+++ WebAPIHotel/Controllers/BookingController.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIHotel.Controllers
+{
+    public class BookingController : ApiController
+    {
+        string cmd = "SELECT * FROM cust_booking LEFT JOIN hotel ON cust_booking.hotel_id = hotel.hotel_id WHERE cust_id = {0};";
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            cmd = String.Format(cmd, id);
+            List<Object> bookings = new SQLHandler().execute(cmd, "Bookings");
+            string jsonResponse = JsonConvert.SerializeObject(bookings);
+            return jsonResponse;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPIHotel/SQLHandler.cs
@@ -0,0 +1,79 @@
+﻿using HotelClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebAPIHotel
+{
+    public class SQLHandler
+    {
+        private static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\George\\source\\repos\\WebAPIHotel\\WebAPIHotel\\App_Data\\Database1.mdf;Integrated Security=True";
+        public SQLHandler()
+        { }
+
+        public List<Object> execute(string cmdString, string requestType)
+        {
+            List<Object> objects = new List<object>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(cmdString, connection))
+                {
+                    using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                    {
+                        switch(requestType)
+                        {
+                            case "Bookings":
+                                return getBookings(reader);
+                                break;
+                        }
+                       
+                    }
+                }
+            }
+            return null;
+        }
+
+        private List<Object> getBookings(SqlDataReader reader)
+        {
+            List<Object> bookings = new List<Object>();
+            while (reader.Read())
+            {
+                Booking b = new Booking
+                {
+                    BookingID = Convert.ToInt32(reader["booking_id"]),
+                    //Replace object instansiation with Flyweight or collection of all hotels/active customers
+                    //Customer = new Customer
+                    //{
+                    //    CustId = Convert.ToInt32(reader["cust_id"]),
+                    //    First_name = reader["cust_first_name"].ToString(),
+                    //    Last_name = reader["cust_last_name"].ToString(),
+                    //    Email = reader["cust_email"].ToString(),
+                    //    Phone_number = reader["cust_phonenumber"].ToString(),
+                    //    DateOfBirth = DateTime.Parse(reader["cust_dob"].ToString())
+                    //},
+                    Hotel = new Hotel
+                    {
+                        HotelID = Convert.ToInt32(reader["hotel_id"]),
+                        HotelName = reader["hotel_name"].ToString(),
+                        HotelPostcode = reader["hotel_postcode"].ToString(),
+                        AddressLine1 = reader["hotel_address1"].ToString(),
+                        AddressLine2 = reader["hotel_address2"].ToString(),
+                        City = reader["hotel_city"].ToString(),
+                    },
+                    DateBookingMade = DateTime.Parse(reader["date_booking_made"].ToString()),
+                    DateOfBooking = DateTime.Parse(reader["date_for_booking"].ToString()),
+                    Activated = (bool)reader["booking_activated"],
+                    HideBooking = (bool)reader["hide_booking"]
+                };
+
+                bookings.Add(b);
+            }
+            return bookings;
+        }
+    }
+}
